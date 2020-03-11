0e34c957aaf00f6dac2e483ba8bc63972629590c
Angel Naguit <101152749@clc330-007m.local>
2020-03-11 12:41:30 -0400

    Initial Commit

---
+++ IOS_Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOS_Project
+//
+//  Created by Angel Naguit on 2020-03-11.
+//  Copyright © 2020 T127-IOS-Project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
