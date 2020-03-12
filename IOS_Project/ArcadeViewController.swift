514a917fcf6d803b067fc7f7d5209f9e7921e007
Aldrin Jacildo <ajacildo1999@gmail.com>
2020-03-12 15:11:30 -0400

    Arcade Mode Functionality
    arcade mode now working,

--- IOS_Project/ArcadeViewController.swift
+++ IOS_Project/ArcadeViewController.swift
@@ -9,13 +9,96 @@
 import UIKit
 
 class ArcadeViewController: UIViewController {
-
+    
+    // VARIABLES
+    var player: Player = Player(name: "");
+    var score = 0;
+    var start_time = 10;
+    var gameTimer: Timer?;
+    var opponentMove = "";
+    var playerMove = "";
+    
+    // IBO OUTLETS
+    @IBOutlet weak var lblTime: UILabel!
+    @IBOutlet weak var txtOpponentMove: UITextField!
+    @IBOutlet weak var lblScore: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.setTimer();
+        self.setMove();
+        self.lblScore.text = String(self.score);
+        
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func PlaySword(_ sender: UIButton) {
+        self.playSword();
+    }
+    
+    @IBAction func PlayShield(_ sender: UIButton) {
+        self.playShield();
+    }
+    
+    @IBAction func PlayCape(_ sender: UIButton) {
+        self.playCape();
+    }
+    
+    private func setTimer() {
+        self.gameTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer) in
+            if self.start_time == 0 {
+                self.gameTimer?.invalidate();
+                self.performSegue(withIdentifier: "saveGame", sender: self);
+            }
+            self.lblTime.text = String(self.start_time);
+            self.start_time -= 1;
+        });
+    }
+    
+    private func playSword(){
+        self.playerMove = self.player.playSword();
+        self.compareMove();
+        self.updateScore();
+    }
+    
+    private func playShield(){
+        self.playerMove = self.player.playShield();
+        self.compareMove();
+        self.updateScore();
+    }
+    
+    private func playCape(){
+        self.playerMove = self.player.playCape();
+        self.compareMove();
+        self.updateScore();
+    }
+    
+    private func compareMove(){
+        // FIRST COMPARISON SHIELD > SWORD > CAPE
+        if self.opponentMove == "Sword" && self.playerMove == "Shield"{
+            self.score += 2;
+        }else if self.opponentMove == "Sword" && self.playerMove == "Cape"{
+            self.score -= 5;
+        }
+        
+        // SECOND COMPARISON CAPE > SHIELD > SWORD
+        if self.opponentMove == "Shield" && self.playerMove == "Cape"{
+            self.score += 1;
+        }else if self.opponentMove == "Shield" && self.playerMove == "Sword"{
+            self.score -= 5;
+        }
+        
+        // THIRD COMPARISON SWORD > CAPE > SHIELD
+        if self.opponentMove == "Cape" && self.playerMove == "Sword"{
+            self.score += 3;
+        }else if self.opponentMove == "Cape" && self.playerMove == "Shield"{
+            self.score -= 5;
+        }
+        
+        self.setMove();
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -23,6 +106,17 @@ class ArcadeViewController: UIViewController {
     
     @IBAction func backBtn(_ sender: UIButton) {
         performSegue(withIdentifier: "backToMain", sender: self);
+    }
+    
+    private func updateScore(){
+        self.lblScore.text = String(self.score);
+    }
+    
+    private func setMove() {
+        let movesets = ["Sword", "Shield", "Cape"];
+        let randomIndex = Int(arc4random_uniform(UInt32(movesets.count)));
+        self.opponentMove = movesets[randomIndex];
+        self.txtOpponentMove.text = self.opponentMove;
     }
     
     /*
--- IOS_Project/SaveGameViewController.swift
+++ IOS_Project/SaveGameViewController.swift
@@ -1,23 +1,23 @@
 //
-//  AboutUsViewController.swift
+//  SaveGameViewController.swift
 //  IOS_Project
 //
-//  Created by Angel Naguit on 2020-03-11.
+//  Created by Aldrin Jacildo on 2020-03-12.
 //  Copyright © 2020 T127-IOS-Project. All rights reserved.
 //
 
 import UIKit
 
-class AboutUsViewController: UIViewController {
-
-    @IBOutlet weak var backBtn: UIButton!
+class SaveGameViewController: UIViewController {
+    
+    @IBAction func BackToMain(_ sender: UIButton) {
+        performSegue(withIdentifier: "backToMain", sender: self);
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-       let arr: [UIButton] = [backBtn];
-        ViewController.applyRounded(arr: arr);
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,10 +25,7 @@ class AboutUsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func backBtn(_ sender: UIButton) {
-            performSegue(withIdentifier: "backToMain", sender: self)
-    }
-    
+
     /*
     // MARK: - Navigation
 
